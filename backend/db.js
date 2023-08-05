1c6ab26ea9c7603553e95e259a85c1e430b32ea1
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-08-04 21:37:55 -0400

    frontend setup

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/"
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongoose succesfully");
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+const connectToMongo = () => {
+    mongoose.connect(mongoURI).then(()=>{
+        console.log('Connected to mongo successfully');
+    }).catch((err)=>{
+        console.log(err);
     })
 }
 module.exports = connectToMongo;
+
