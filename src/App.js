c5b5163bf7418e611a4a8208fb2f283e1405360c
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-08-07 15:43:50 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+// import logo from './logo.svg';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-import './App.css';
+
+import {About} from "./components/About";
+import { Home } from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+   
+    </>
   );
 }
 
