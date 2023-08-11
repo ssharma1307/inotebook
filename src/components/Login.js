45711920d3c305c93a69699263d36c7b029f0816
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-08-11 13:21:49 -0400

    login page

---
+++ src/components/Login.js
@@ -0,0 +1,74 @@
+import React,{useState} from "react";
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+
+    const [credentials, setCredentials] = useState({email:"", password:""});
+    let history = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    fetch("");
+    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email:credentials.email, password:credentials.password}),
+    });
+    
+    const json = await response.json();
+    console.log(json);
+    if(json.success){
+            //save the auth token and redirect
+            localStorage.setItem('token', json.authtoken);
+            history.push("/");
+    }
+    else{
+        alert("Invalid credentials")
+    }
+  }
+
+  const onChange = (e)=>{
+    setCredentials({...credentials, [e.target.name]:e.target.value})
+}
+  return (
+    <div>
+      <form  onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input value={credentials.email} type="email" className="form-control" id="email" name="email"  onChange={onChange}aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+          onChange={onChange}
+          value={credentials.password}
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+         
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
